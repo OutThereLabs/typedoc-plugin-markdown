ef2add071b6d91d700ce043bca1498d2e69a6148
Josh Pyles <joshpyles@gmail.com>
2018-02-28 17:25:54 -0800

    Add relationships section

--- src/theme/helpers/compileMember.ts
+++ src/theme/helpers/compileMember.ts
@@ -11,32 +11,32 @@ export function compileMember(member: DeclarationReflection) {
   const options = ThemeService.getOptions();
 
   let md = '';
-
+  let hasRelationships = (member.implementedTypes || member.implementationOf || member.overwrites)
   if (!(options.excludePrivate && member.flags.isPrivate) && !member.inheritedFrom) {
     switch (member.kind) {
       case ReflectionKind.Constructor:
-        md = ThemeService.compilePartial('member.constructor.hbs', member);
+        md = ThemeService.compilePartial('member.constructor.hbs', Object.assign(member, { hasRelationships }));
         break;
       case ReflectionKind.ObjectLiteral:
-        md = ThemeService.compilePartial('member.object.hbs', member);
+        md = ThemeService.compilePartial('member.object.hbs', Object.assign(member, { hasRelationships }));
         break;
       case ReflectionKind.ExternalModule:
-        md = ThemeService.compilePartial('member.object.hbs', member);
+        md = ThemeService.compilePartial('member.object.hbs', Object.assign(member, { hasRelationships }));
         break;
       case ReflectionKind.Property:
-        md = ThemeService.compilePartial('member.property.hbs', member);
+        md = ThemeService.compilePartial('member.property.hbs', Object.assign(member, { hasRelationships }));
         break;
       case ReflectionKind.Method:
-        md = ThemeService.compilePartial('member.method.hbs', member);
+        md = ThemeService.compilePartial('member.method.hbs', Object.assign(member, { hasRelationships }));
         break;
       case ReflectionKind.Class:
-        md = ThemeService.compilePartial('member.class.hbs', member);
+        md = ThemeService.compilePartial('member.class.hbs', Object.assign(member, { hasRelationships }));
         break;
       case ReflectionKind.Function:
-        md = ThemeService.compilePartial('member.function.hbs', member);
+        md = ThemeService.compilePartial('member.function.hbs', Object.assign(member, { hasRelationships }));
         break;
       default:
-        md = ThemeService.compilePartial('member.hbs', member);
+        md = ThemeService.compilePartial('member.hbs', Object.assign(member, { hasRelationships }));
     }
   }
 
